abf3982fa03ddaa8a63ea8db9c23d7d29be146ab
Diana Kovacheva <dmarinova@outlook.com>
2023-11-14 23:36:13 +0100

    Show register form instead login form

---
+++ src/components/formHolder/SideFormsView.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import FormFooter from "./FormFooter";
+import LoginForm from "../loginForm/LoginForm";
+import RegisterForm from "../registerForm/RegisterForm";
+
+export default function SideForms() {
+  return (
+    <Grid container component="main" sx={{ height: "100vh" }}>
+      <CssBaseline />
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage:
+            "url(../../../public/images/tennis-sign-in-page.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        {/* <LoginForm /> */}
+        <RegisterForm />
+        <FormFooter />
+      </Grid>
+    </Grid>
+  );
+}
