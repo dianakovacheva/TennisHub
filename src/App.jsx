0bbe903cb1ed348f2d34232ac8c15350b1375bee
Diana Kovacheva <dmarinova@outlook.com>
2023-11-21 14:13:52 +0100

    Add auth provider to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import SideForms from "./components/formHolder/SideFormsView";
 import Home from "./components/home/Home";
 import PageNotFound from "./components/pageNotFound/PageNotFound";
 import Calendar from "./components/calendar/Calendar";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <>
-      <Navigation />
-      {/* <SideForms /> */}
-      <Home />
-      {/* <Calendar /> */}
-      {/* <PageNotFound /> */}
-      <Footer />
-    </>
+    <AuthProvider>
+      <>
+        <Navigation />
+        {/* <SideForms /> */}
+        <Home />
+        {/* <Calendar /> */}
+        {/* <PageNotFound /> */}
+        <Footer />
+      </>
+    </AuthProvider>
   );
 }
 
