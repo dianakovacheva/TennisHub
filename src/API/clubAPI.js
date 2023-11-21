d8928d6cab83b1c74979074ec9e8ab999f7b587c
Diana Kovacheva <dmarinova@outlook.com>
2023-11-21 11:57:00 +0100

    Edit club base url

--- src/API/clubAPI.js
+++ src/API/clubAPI.js
@@ -1,8 +1,8 @@
-const baseURL = "http://localhost:3000/api/clubs/";
+const clubBaseURL = "http://localhost:3000/api/club/";
 
 export const getAllClubs = async () => {
   try {
-    const response = await fetch(baseURL);
+    const response = await fetch(clubBaseURL);
     const result = await response.json();
 
     const clubData = Object.values(result);
