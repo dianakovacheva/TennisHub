005d510ffd06dd233968b49d0f27c564d6430305
Diana Kovacheva <dmarinova@outlook.com>
2023-11-23 21:33:46 +0100

    Update AuthContext file

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,45 +1,60 @@
-import { Component, createContext, useContext } from "react";
-import { useLocalStorage } from "../hooks/useLocalStorage";
+import { createContext, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import useLocalStorage from "../hooks/useLocalStorage";
+import * as userAPI from "../API/userAPI";
+import Endpoint from "../endPoints";
 
-export const AuthContext = createContext();
+const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+
   const [auth, setAuth] = useLocalStorage("auth", {});
 
-  const userLogin = (authData) => {
-    setAuth(authData);
+  const loginSubmitHandler = async (values) => {
+    const result = await userAPI.login(values.email, values.password);
+
+    setAuth(result);
+
+    localStorage.setItem("accessToken", result.accessToken);
+
+    navigate(Endpoint.Home);
   };
 
-  const userLogout = () => {
+  const registerSubmitHandler = async (values) => {
+    const result = await userAPI.register(
+      values.firstName,
+      values.lastName,
+      values.email,
+      values.password
+    );
+
+    setAuth(result);
+
+    localStorage.setItem("accessToken", result.accessToken);
+
+    navigate(Endpoint.Home);
+  };
+
+  const logoutHandler = () => {
     setAuth({});
+
+    localStorage.removeItem("accessToken");
   };
 
-  return (
-    <AuthContext.Provider
-      value={{
-        user: auth,
-        userLogin,
-        userLogout,
-        isAuthenticated: !!auth.accessToken,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-export const useAuthContext = () => {
-  const context = useContext(AuthContext);
-
-  return context;
-};
-
-export const withAuth = (Component) => {
-  const authWrapper = (props) => {
-    const context = useContext(AuthContext);
-
-    return <Component {...props} auth={context} />;
+  const values = {
+    loginSubmitHandler,
+    registerSubmitHandler,
+    logoutHandler,
+    username: (auth.firstName && auth.lastName) || auth.email,
+    email: auth.email,
+    userId: auth._id,
+    isAuthenticated: !!auth.token,
   };
 
-  return authWrapper;
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
+
+AuthContext.displayName = "AuthContext";
+
+export default AuthContext;
