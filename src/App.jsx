232dcb178856967088ae846c355235ed79b875f4
Diana Kovacheva <dmarinova@outlook.com>
2023-11-25 17:34:08 +0100

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+
+import { AuthProvider } from "./contexts/AuthContext";
+
+import Home from "./components/home/Home";
 import Navigation from "./components/header/Navigation";
 import Footer from "./components/footer/Footer";
-import SideForms from "./components/formHolder/SideFormsView";
-import Home from "./components/home/Home";
 import PageNotFound from "./components/pageNotFound/PageNotFound";
+import Register from "./components/register/Register";
+import Login from "./components/login/Login";
+import Logout from "./components/logout/Logout";
 import Calendar from "./components/calendar/Calendar";
-import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <AuthProvider>
       <>
         <Navigation />
-        {/* <SideForms /> */}
-        <Home />
-        {/* <Calendar /> */}
-        {/* <PageNotFound /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/book-court" element={<Calendar />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
         <Footer />
       </>
     </AuthProvider>
