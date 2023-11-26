bd398c2a53aaf7b943633d74ed720e25e0822b4f
Diana Kovacheva <dmarinova@outlook.com>
2023-11-26 14:21:45 +0100

    Add court related function to communicate with the backend

---
+++ src/API/courtAPI.js
@@ -0,0 +1,72 @@
+import * as request from "../lib/request";
+
+const courtBaseUrl = "http://localhost:3000/api/club/court";
+
+// Create court //
+export const createCourt = async (
+  clubId,
+  courtName,
+  surface,
+  indoor,
+  lighting
+) => {
+  try {
+    const courtData = await request.post(`${courtBaseUrl}/create`, {
+      clubId,
+      courtName,
+      surface,
+      indoor,
+      lighting,
+    });
+
+    return courtData;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+// Get court by id //
+export const getCourtById = async (courtId) => {
+  try {
+    const courtData = await request.get(`${courtBaseUrl}/${courtId}`);
+
+    return courtData;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+// Edit court //
+export const editCourt = async (
+  clubId,
+  courtName,
+  surface,
+  indoor,
+  lighting
+) => {
+  try {
+    const courtData = await request.put(
+      `${courtBaseUrl}/${courtId}/edit`,
+      clubId,
+      courtName,
+      surface,
+      indoor,
+      lighting
+    );
+
+    return courtData;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+// Delete court //
+export const deleteCourt = async (courtId) => {
+  try {
+    const court = await request.delete(`${courtBaseUrl}/${courtId}/delete`);
+
+    return court;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
