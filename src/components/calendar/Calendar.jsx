83631930b38fabf4672cdb980e69d567a087efa7
Diana Kovacheva <dmarinova@outlook.com>
2023-11-26 14:24:27 +0100

    Remove hardcoded courts and bookings data

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,41 +1,22 @@
-import { React, useEffect, useRef, useCallback, useMemo } from "react";
+import {
+  React,
+  useEffect,
+  useRef,
+  useCallback,
+  useMemo,
+  useState,
+} from "react";
 import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+import "moment/locale/de";
 
 import "./Calendar.css";
+import { getAllBookings } from "../../API/bookingAPI";
+import { getClubCourts } from "../../API/clubAPI";
 
 const localizer = momentLocalizer(moment);
 
-const bookings = [
-  {
-    id: 1,
-    title: "Sergey's Birthday",
-    start: moment("2023-11-15T08:00:00").toDate(),
-    end: moment("2023-11-15T20:00:00").toDate(),
-    resourceId: 1,
-  },
-  {
-    id: 2,
-    title: "Court 1",
-    start: moment("2023-11-16T12:00:00").toDate(),
-    end: moment("2023-11-16T13:00:00").toDate(),
-    resourceId: 2,
-  },
-  {
-    id: 2,
-    title: "Court 3",
-    start: moment("2023-11-16T09:00:00").toDate(),
-    end: moment("2023-11-16T12:30:00").toDate(),
-    resourceId: 3,
-  },
-];
 
-const courts = [
-  { courtId: 1, title: "1" },
-  { courtId: 2, title: "2" },
-  { courtId: 3, title: "3" },
-  { courtId: 4, title: "4" },
-];
 
 export default function Calendar(props) {
   return (
@@ -48,10 +29,6 @@ export default function Calendar(props) {
         titleAccessor="title"
         defaultView="day"
         views={["day"]}
-        min={moment("2023-11-15T07:00:00").toDate()}
-        max={moment("2023-11-15T21:30:00").toDate()}
-        resources={courts}
-        resourceIdAccessor="courtId"
         resourceTitleAccessor="title"
         formats={{
           dayHeaderFormat: (date) => moment(date).format("dddd, D MMMM Y"),
