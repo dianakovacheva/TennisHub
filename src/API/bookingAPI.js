6b815e329171370368efb0eb7b37fc71ea7a55da
Diana Kovacheva <dmarinova@outlook.com>
2023-11-26 14:19:07 +0100

    Rename variables

--- src/API/bookingAPI.js
+++ src/API/bookingAPI.js
@@ -2,38 +2,37 @@ import * as request from "../lib/request";
 
 const bookingBaseUrl = "http://localhost:3000/api/booking";
 
+// Book court //
 export const bookCourt = async (courtId, startTime, endTime, players) => {
   try {
-    const response = await request.post(`${bookingBaseUrl}/book-court`, {
+    const bookingData = await request.post(`${bookingBaseUrl}/book-court`, {
       courtId,
       startTime,
       endTime,
       players,
     });
 
-    return response;
+    return bookingData;
   } catch (error) {
     throw new Error(error.message);
   }
 };
 
+// Get all bookings //
 export const getAllBookings = async () => {
   try {
-    const response = await fetch(bookingBaseUrl);
-    const result = await response.json();
-    const allBookingsData = Object.values(result);
+    const allBookings = await request.get(bookingBaseUrl);
 
-    return allBookingsData;
+    return allBookings;
   } catch (error) {
     throw new Error(error.message);
   }
 };
 
+// Get booking by id //
 export const getBookingById = async (bookingId) => {
   try {
-    const response = await fetch(`${bookingBaseUrl}/${bookingId}`);
-    const result = await response.json();
-    const bookingData = Object.values(result);
+    const bookingData = await request.get(`${bookingBaseUrl}/${bookingId}`);
 
     return bookingData;
   } catch (error) {
