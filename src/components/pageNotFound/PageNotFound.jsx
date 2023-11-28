563425ac68375730a05a30aaaabfbb14ad4dc3bf
Diana Kovacheva <dmarinova@outlook.com>
2023-11-28 12:28:59 +0100

    Import Link from react-router-dom and add it to the "Home Page" button

--- src/components/pageNotFound/PageNotFound.jsx
+++ src/components/pageNotFound/PageNotFound.jsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import "./PageNotFound.css";
 
+import { Link } from "react-router-dom";
+
 export default function PageNotFound() {
   return (
     <div className="section-container">
@@ -9,13 +11,11 @@ export default function PageNotFound() {
         <h1 className="heading">404</h1>
         <p className="oops">Oops...</p>
         <p className="text">We&apos;re sorry, but something went wrong.</p>
-        <Button
-          variant="contained"
-          className="btn btn-secondary orange"
-          href="/"
-        >
-          Home Page
-        </Button>
+        <Link to="/">
+          <Button variant="contained" className="btn btn-secondary orange">
+            Home Page
+          </Button>
+        </Link>
       </section>
     </div>
   );
