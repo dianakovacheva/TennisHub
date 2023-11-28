0f4342be5af3ed34dbb1cc8c79e4da670a8a0814
Diana Kovacheva <dmarinova@outlook.com>
2023-11-28 11:55:07 +0100

    Add routes as children to GuestHeader and UserHeader components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 
-import { AuthProvider } from "./contexts/AuthContext";
-
 import Home from "./components/home/Home";
-import Navigation from "./components/header/Navigation";
+import GuestHeader from "./components/headers/guest/GuestHeader";
+import UserHeader from "./components/headers/user/UserHeader";
 import Footer from "./components/footer/Footer";
 import PageNotFound from "./components/pageNotFound/PageNotFound";
 import Register from "./components/register/Register";
@@ -11,22 +10,31 @@ import Login from "./components/login/Login";
 import Logout from "./components/logout/Logout";
 import Calendar from "./components/calendar/Calendar";
 
+import { useContext } from "react";
+import AuthContext from "./contexts/AuthContext";
+
 function App() {
+  const { isAuthenticated } = useContext(AuthContext);
+  const routes = (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/book-court" element={<Calendar />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+
   return (
-    <AuthProvider>
-      <>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/book-court" element={<Calendar />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-        <Footer />
-      </>
-    </AuthProvider>
+    <>
+      {!isAuthenticated && <GuestHeader>{routes}</GuestHeader>}
+      {isAuthenticated && <UserHeader>{routes}</UserHeader>}
+
+      <Footer />
+    </>
   );
 }
 
