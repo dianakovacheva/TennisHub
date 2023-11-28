37431a2368caff937432ede2b046cc175a279f8f
Diana Kovacheva <dmarinova@outlook.com>
2023-11-28 11:56:39 +0100

    Remove min height from footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,7 +12,7 @@ export default function StickyFooter() {
       sx={{
         display: "flex",
         flexDirection: "column",
-        minHeight: "30vh",
+        // minHeight: auto,
       }}
     >
       <CssBaseline />
