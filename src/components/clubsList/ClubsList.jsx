f90288a0e2a1e6d37ccb56f53dce2666faa3ac52
Diana Kovacheva <dmarinova@outlook.com>
2023-11-28 23:28:28 +0100

    Import clubsListItemCard component

--- src/components/clubsList/ClubsList.jsx
+++ src/components/clubsList/ClubsList.jsx
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 
-import ClubsListItemCard from "./clubsListItem/ClubsListItemCard";
 import { getAllClubs } from "../../API/clubAPI";
+import ClubsListItemCard from "./clubsListItem/ClubsListItemCard";
 import ClubsListCSS from "./ClubsList.module.css";
 
 export default function ClubsList() {
