f80e769b7d29947e8f5d4bafac2561ff2c271ebd
Diana Kovacheva <dmarinova@outlook.com>
2023-11-29 13:50:10 +0100

    Add statistics card

--- src/components/home/user/UserHome.jsx
+++ src/components/home/user/UserHome.jsx
@@ -1,13 +1,41 @@
-import { React, useState, useEffect } from "react";
+import { useContext } from "react";
 
-import ClubsList from "../../clubsList/ClubsList";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import GroupsIcon from "@mui/icons-material/Groups";
+
+import UserHomeCSS from "../user/UserHome.module.css";
+
+import AuthContext from "../../../contexts/AuthContext";
+import StatisticCard from "./statisticCard/StatisticCard";
 
 export default function UserHome() {
+  const { userCreatedClubs } = useContext(AuthContext);
+
   return (
-    <>
-      {/* Start Clubs Gallery */}
-      <ClubsList />
-      {/* End Clubs Gallery */}
-    </>
+    <Box className={UserHomeCSS.statisticCardsSection}>
+      <StatisticCard />
+      <Card className={UserHomeCSS.cardHolder}>
+        <Box>
+          <CardContent className={UserHomeCSS.cardContentLeft}>
+            <Typography component="div" variant="h3">
+              {userCreatedClubs.length}
+            </Typography>
+            <Typography color="text.secondary" component="p" variant="h6">
+              Created Clubs
+            </Typography>
+          </CardContent>
+        </Box>
+        <Box>
+          <CardContent className={UserHomeCSS.cardContentRight}>
+            <div>
+              <GroupsIcon className={UserHomeCSS.groupsIcon} />
+            </div>
+          </CardContent>
+        </Box>
+      </Card>
+    </Box>
   );
 }
