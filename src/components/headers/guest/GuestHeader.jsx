08f597f9276e946af3a3319793b92a6548f77d7f
Diana Kovacheva <dmarinova@outlook.com>
2023-11-29 16:01:22 +0100

    Remove unneeded lines of code

--- src/components/headers/guest/GuestHeader.jsx
+++ src/components/headers/guest/GuestHeader.jsx
@@ -1,4 +1,3 @@
-import React, { useContext } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -8,27 +7,13 @@ import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 
-import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
-
 import { Link } from "react-router-dom";
 
 import Search from "../../search/Search";
-import AuthContext from "../../../contexts/AuthContext";
 
-import GuestGuestHeaderCSS from "./GuestHeader.module.css";
-
-const pages = ["Courts", "Blog"];
-const guestNav = [
-  { label: "Register", link: "/register" },
-  { label: "Login", link: "/login" },
-  { label: "Clubs", link: "/clubs" },
 ];
 
-const dropdownMenu = ["Profile", "Create Club", "Logout"];
-
 export default function GuestHeader({ children }) {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -44,97 +29,22 @@ export default function GuestHeader({ children }) {
         <Container maxWidth="xl">
           <Toolbar disableGutters>
             {/* Start App Logo */}
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontWeight: 700,
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              TennisHub
-            </Typography>
             {/* End App Logo */}
 
             {/* Start Mobile GuestHeader */}
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
             </Box>
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontWeight: 700,
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              TennisHub
-            </Typography>
             {/* End Mobile GuestHeader */}
 
             {/* Start GuestHeader Links */}
             <Box
-              className={GuestGuestHeaderCSS.navigationLinks}
               sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
             >
               {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
-                  {page}
-                </Button>
               ))}
             </Box>
             {/* End GuestHeader Links */}
 
             {/* Start Search */}
-            <Search />
             {/* End Search */}
           </Toolbar>
         </Container>
