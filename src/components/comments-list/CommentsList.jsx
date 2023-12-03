4e57824a165630446b94402a0f3760ef80e222cf
Diana Kovacheva <dmarinova@outlook.com>
2023-12-03 13:43:54 +0100

    Add CommentsList module

---
+++ src/components/comments-list/CommentsList.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { Container, Typography } from "@mui/material";
+
+import { getClubComments } from "../../API/clubAPI";
+import CommentItemCard from "./comment-item-card/CommentItemCard";
+import CommentsListCSS from "./CommentsList.module.css";
+import NoComments from "../no-comments/NoComments";
+
+export default function CommentsList() {
+  const [comments, setComments] = useState([]);
+  const { clubId } = useParams();
+
+  useEffect(() => {
+    getClubComments(clubId)
+      .then((result) => setComments(result))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [clubId]);
+
+  return (
+    <Container className={CommentsListCSS.commentsContainer}>
+      <Typography className={CommentsListCSS.commentHeader}>
+        User Comments
+      </Typography>
+
+      {comments.map((commentObject) => (
+        <CommentItemCard
+          key={commentObject._id}
+          commentObject={commentObject}
+        />
+      ))}
+
+      {comments.length === 0 && <NoComments />}
+    </Container>
+  );
+}
