3f97b262229b4bfb994d7bf4af1452add2fbd55a
Diana Kovacheva <dmarinova@outlook.com>
2023-12-03 13:59:26 +0100

    Update css styles

--- src/components/club-details-card/ClubDetailsCard.jsx
+++ src/components/club-details-card/ClubDetailsCard.jsx
@@ -88,7 +88,6 @@ export default function ClubDetailsCard() {
           </Button>
         </CardActions>
       </Card>
-      <Divider className={ClubDetailsCardCSS.divider} variant="middle" />
     </>
   );
 }
--- src/components/club-details/ClubDetails.jsx
+++ src/components/club-details/ClubDetails.jsx
@@ -2,12 +2,15 @@ import AddComment from "../add-comment/AddComment";
 import ClubDetailsCard from "../club-details-card/ClubDetailsCard";
 import CommentsList from "../comments-list/CommentsList";
 
+import { Divider } from "@mui/material";
+
 import ClubDetailsCSS from "./ClubDetails.module.css";
 
 export default function ClubDetails() {
   return (
     <div className={ClubDetailsCSS.detailsPageContainer}>
       <ClubDetailsCard />
+      <Divider className={ClubDetailsCSS.divider} variant="middle" />
       <AddComment />
       <CommentsList />
     </div>
