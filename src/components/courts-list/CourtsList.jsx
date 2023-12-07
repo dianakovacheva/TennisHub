62f79cb1c12a1c0156b17cd9294f54aa8c67c548
Diana Kovacheva <dmarinova@outlook.com>
2023-12-07 20:03:18 +0100

    Receive object with props

--- src/components/courts-list/CourtsList.jsx
+++ src/components/courts-list/CourtsList.jsx
@@ -1,36 +1,26 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-
 import { Container, Typography } from "@mui/material";
 
-import * as clubAPI from "../../API/clubAPI";
-import CourtItemCard from "./court-item-card/CourtItemCard";
+import CourtsTable from "./courts-table/CourtsTable";
 
 import CourtsListCSS from "./CourtsList.module.css";
 import NoCourts from "../no-courts/NoCourts";
 
-export default function CourtsList() {
-  const [courts, setCourts] = useState([]);
-  const { clubId } = useParams();
-
-  useEffect(() => {
-    clubAPI
-      .getClubCourts(clubId)
-      .then((result) => setCourts(result))
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [clubId]);
-
+export default function CourtsList({
+  isClubOwner,
+  courts,
+  requestRefreshHandler,
+}) {
   return (
     <Container className={CourtsListCSS.courtsContainer}>
       <Typography className={CourtsListCSS.courtHeader}>
         Courts ({courts.length})
       </Typography>
 
-      {courts.map((courtObject) => (
-        <CourtItemCard key={courtObject._id} courtObject={courtObject} />
-      ))}
+      <CourtsTable
+        isClubOwner={isClubOwner}
+        courts={courts}
+        requestRefreshHandler={requestRefreshHandler}
+      />
 
       {courts.length === 0 && <NoCourts />}
     </Container>
