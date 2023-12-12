3ae9feb2a6c6b66eeaf8e4249da7828685055ef0
Diana Kovacheva <dmarinova@outlook.com>
2023-12-12 14:54:22 +0100

    Add condition: only club owners can edit a booking

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,25 +1,24 @@
 import { React, useEffect, useState, useContext } from "react";
 import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
-import { useNavigate } from "react-router-dom";
+
 import moment from "moment";
 import "moment/locale/de";
 
-import "./Calendar.css";
-import { getAllBookings } from "../../API/bookingAPI";
-import { getClubCourts } from "../../API/clubAPI";
 import { useParams } from "react-router-dom";
+import { useSnackbar } from "../../contexts/SnackbarContext";
+
+import "./Calendar.css";
+
 import BookingModal from "../booking-modal/BookingModal";
 import AuthContext from "../../contexts/AuthContext";
 
 import * as clubAPI from "../../API/clubAPI";
 import * as bookingAPI from "../../API/bookingAPI";
 
-import { useSnackbar } from "../../contexts/SnackbarContext";
-
 const localizer = momentLocalizer(moment);
 
 export default function Calendar() {
-  const navigate = useNavigate();
+  const [clubData, setClubData] = useState({});
   const [bookings, setBookings] = useState([]);
   const [courts, setCourts] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -36,7 +35,8 @@ export default function Calendar() {
   const { clubId } = useParams();
 
   useEffect(() => {
-    getAllBookings()
+    bookingAPI
+      .getAllBookings()
       .then((result) =>
         setBookings(
           result.map((booking) => ({
@@ -58,12 +58,18 @@ export default function Calendar() {
       .catch((error) => {
         console.log(error);
       });
-    console.log("In useEffect");
   }, [courts, toggleRefresh]);
 
   useEffect(() => {
     Promise.all([
-      getClubCourts(clubId)
+      clubAPI
+        .getClubById(clubId)
+        .then((result) => setClubData(result))
+        .catch((error) => {
+          console.log(error);
+        }),
+      clubAPI
+        .getClubCourts(clubId)
         .then((result) =>
           setCourts(result.sort((a, b) => a.courtName - b.courtName))
         )
@@ -84,10 +90,14 @@ export default function Calendar() {
 
   let numCourts = courts.length;
 
+  let isClubOwner;
+
+  if (clubData.manager != undefined) {
+    isClubOwner = clubData.manager[0]?._id === userId ? true : false;
+  }
+
   const requestRefreshHandler = () => {
-    console.log(toggleRefresh);
     setToggleRefresh(!toggleRefresh);
-    console.log(toggleRefresh);
   };
 
   const onSelectEvent = (e) => {
@@ -96,9 +106,11 @@ export default function Calendar() {
       (booking) => booking._id == bookingId
     );
 
-    setEditMode(true);
-    setSelectedBooking(selectedBooking);
-    setIsModalOpen(true);
+    if (isClubOwner) {
+      setEditMode(true);
+      setSelectedBooking(selectedBooking);
+      setIsModalOpen(true);
+    }
   };
 
   // Book Court
@@ -141,83 +153,78 @@ export default function Calendar() {
   };
 
   return (
-    <>
-      <div className="calendar-container">
-        {!numCourts && (
-          <div>
-            <p>No courts yet</p>
-          </div>
-        )}
-        {!!numCourts && (
-          <>
-            <BigCalendar
-              culture="de-DE"
-              localizer={localizer}
-              events={bookings}
-              selectable
-              onSelectSlot={(slotInfo) => {
-                const clickedEvent = {
-                  ...slotInfo,
-                  courtId: slotInfo.resourceId,
-                  court: courts.find(
-                    (court) => court._id == slotInfo.resourceId
-                  ),
-                };
-                setSelectedEvent(clickedEvent);
-                setSelectedCourt(clickedEvent.court);
-                setEditMode(false);
-                setIsModalOpen(true);
-              }}
-              onSelectEvent={onSelectEvent}
-              startAccessor="start"
-              endAccessor="end"
-              titleAccessor="title"
-              defaultView="day"
-              views={["day"]}
-              step={60}
-              timeslots={1}
-              min={moment("1972-01-01T07:00:00").toDate()}
-              max={moment("2100-12-31T21:30:00").toDate()}
-              resources={courts.map((court) => ({
-                id: court._id,
-                title: court.courtName,
-              }))}
-              resourceIdAccessor="id"
-              resourceTitleAccessor="title"
-              formats={{
-                dayHeaderFormat: (date) =>
-                  moment(date).format("dddd, D MMMM Y"),
-                eventTimeRangeFormat: ({ start, end }, culture, localizer) =>
-                  localizer.format(start, "HH:mm", culture) +
-                  " - " +
-                  localizer.format(end, "HH:mm", culture),
-                timeGutterFormat: (date, culture, localizer) =>
-                  localizer.format(date, "HH:mm", culture),
-              }}
-              messages={{
-                next: "→",
-                previous: "←",
-              }}
+    <div className="calendar-container">
+      {!numCourts && (
+        <div>
+          <p>No courts yet</p>
+        </div>
+      )}
+      {!!numCourts && (
+        <>
+          <BigCalendar
+            culture="de-DE"
+            localizer={localizer}
+            events={bookings}
+            selectable
+            onSelectSlot={(slotInfo) => {
+              const clickedEvent = {
+                ...slotInfo,
+                courtId: slotInfo.resourceId,
+                court: courts.find((court) => court._id == slotInfo.resourceId),
+              };
+              setSelectedEvent(clickedEvent);
+              setSelectedCourt(clickedEvent.court);
+              setEditMode(false);
+              setIsModalOpen(true);
+            }}
+            onSelectEvent={onSelectEvent}
+            startAccessor="start"
+            endAccessor="end"
+            titleAccessor="title"
+            defaultView="day"
+            views={["day"]}
+            step={60}
+            timeslots={1}
+            min={moment("1972-01-01T07:00:00").toDate()}
+            max={moment("2100-12-31T21:30:00").toDate()}
+            resources={courts.map((court) => ({
+              id: court._id,
+              title: court.courtName,
+            }))}
+            resourceIdAccessor="id"
+            resourceTitleAccessor="title"
+            formats={{
+              dayHeaderFormat: (date) => moment(date).format("dddd, D MMMM Y"),
+              eventTimeRangeFormat: ({ start, end }, culture, localizer) =>
+                localizer.format(start, "HH:mm", culture) +
+                " - " +
+                localizer.format(end, "HH:mm", culture),
+              timeGutterFormat: (date, culture, localizer) =>
+                localizer.format(date, "HH:mm", culture),
+            }}
+            messages={{
+              next: "→",
+              previous: "←",
+            }}
+          />
+          {currUser?._id && (
+            <BookingModal
+              isOpen={isModalOpen}
+              onClose={() => setIsModalOpen(false)}
+              bookCourt={bookCourt}
+              editBooking={editBooking}
+              deleteBooking={deleteBooking}
+              selectedCourt={selectedCourt}
+              selectedEvent={selectedEvent}
+              players={clubMembers}
+              currUser={currUser}
+              requestRefreshHandler={requestRefreshHandler}
+              editMode={editMode}
+              selectedBooking={selectedBooking}
             />
-            {currUser?._id && (
-              <BookingModal
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-                bookCourt={bookCourt}
-                editBooking={editBooking}
-                deleteBooking={deleteBooking}
-                selectedCourt={selectedCourt}
-                selectedEvent={selectedEvent}
-                players={clubMembers}
-                currUser={currUser}
-                requestRefreshHandler={requestRefreshHandler}
-                editMode={editMode}
-                selectedBooking={selectedBooking}
-              />
-            )}
-          </>
-        )}
-      </div>
-    </>
+          )}
+        </>
+      )}
+    </div>
   );
 }
